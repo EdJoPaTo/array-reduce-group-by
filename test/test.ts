fc990f2b38b9a0f2781865301cd9866e2d33ea2d
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-12-10 14:39:10 +0100

    refactor: ensure typesafe array access

--- test/test.ts
+++ test/test.ts
@@ -3,13 +3,13 @@ import arrayReduceGroupBy from '../source';
 
 test('empty input array', t => {
 	const input: string[] = [];
-	const result = input.reduce(arrayReduceGroupBy(o => o[0]), {});
+	const result = input.reduce(arrayReduceGroupBy(o => o[0]!), {});
 	t.deepEqual(result, {});
 });
 
 test('simple example', t => {
 	const input: string[] = ['Alpha', 'Beta', 'Animal'];
-	const result = input.reduce(arrayReduceGroupBy(o => o[0]), {});
+	const result = input.reduce(arrayReduceGroupBy(o => o[0]!), {});
 	t.deepEqual(result, {
 		A: ['Alpha', 'Animal'],
 		B: ['Beta']
